d0160f8b190d5fe1d4e380b347193300eca54a50
siva <s69prasad82@gmail.com>
2021-01-24 18:46:29 +0530

    \

--- file1.c
+++ file1.c
@@ -3,6 +3,8 @@ void main()
 {
 	int a[]={2,4,6,8};
 	int i;
+	int n=(sizeof(a)/sizeof(a[0]));
+
 	for(i=5;i>2;i--)
 	{
 		int temp=a[i-1];
--- file2.c
+++ file2.c
@@ -4,6 +4,7 @@ void main()
 	int arr[]={10,20,30,40,50};
 	int sum=0;
 	int i;
+	int n=(sizeof(a)/sizeof(a[0]));
 	for(i=0;i<5;i++)
 	{
 		sum=sum+arr[i];
