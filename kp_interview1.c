cb703265c2b8a6a16d359801e76ecc1fbdae3a5a
s69prasad82 <77826951+s69prasad82@users.noreply.github.com>
2021-02-03 18:14:56 +0530

    kp_interview1.c
    interview question

---
+++ kp_interview1.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+void func1(void); 
+main() 
+{ 
+int count; 
+for (count = 0; count < 4; count++) 
+{ 
+printf("At iteration %d: ", count); 
+func1(); 
+}
+ return 0; 
+} 
+void func1(void)
+ { 
+static int x = 0;
+ int y = 0; 
+printf("x = %d, y = %d\n", x++, y++);
+ }
+
